8e5e342edf116f026abfefddfa459bc6a1720149
Jason Cabrera <cabrerajason7@gmail.com>
2020-12-30 00:18:28 -0800

    added console logs for env variables

--- server/app.js
+++ server/app.js
@@ -3,6 +3,13 @@ const app = express();
 const path = require('path');
 const cors = require('cors');
 require('dotenv').config()
+console.log(process.env.DB_API_KEY);
+console.log(process.env.DB_AUTH_DOMAIN);
+console.log(process.env.DB_PROJECT_ID);
+console.log(process.env.DB_STORAGE_BUCKET);
+console.log(process.env.DB_MESSAGING_SENDER_ID);
+console.log(process.env.DB_APP_ID);
+console.log(process.env.DB_MEASUREMENT_ID);
 
 const port = process.env.PORT || 9000;
 
