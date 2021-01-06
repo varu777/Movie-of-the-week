e5bb8d34628c469916a35c17e17d87029265edc4
Jason Cabrera <cabrerajason7@gmail.com>
2021-01-05 21:08:08 -0800

    added firebase auth

--- server/db/index.js
+++ server/db/index.js
@@ -1,4 +1,5 @@
 const firebase = require('firebase');
+var admin = require("firebase-admin");
 require('dotenv').config()
 
 // Initialize Cloud Firestore through Firebase
@@ -11,8 +12,24 @@ firebase.initializeApp({
     appId: process.env.DB_APP_ID, 
     measurementId: process.env.DB_MEASUREMENT_ID
 });
-  
+
+admin.initializeApp({
+  credential: admin.credential.cert(
+    {
+        "type": "service_account",
+        "project_id": process.env.DB_PROJECT_ID,
+        "private_key_id": process.env.DB_PRIVATE_KEY_ID,
+        "private_key": process.env.DB_PRIVATE_KEY,
+        "client_email": process.env.DB_CLIENT_EMAIL,
+        "client_id": process.env.DB_CLIENT_ID, 
+        "auth_uri": process.env.DB_AUTH_URI,
+        "token_uri": process.env.DB_AUTH_TOKEN_URI,
+        "auth_provider_x509_cert_url": process.env.DB_AUTH_PROVIDER,
+        "client_x509_cert_url": process.env.DB_CLIENT_CERT_URL
+    }),
+  databaseURL: "https://moviepicker-33a06-default-rtdb.firebaseio.com"
+});
+
 const db = firebase.firestore();
-db.enablePersistence()
 
 module.exports = db;
