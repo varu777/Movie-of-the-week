d148248536aba4a92d777b1a14cefcb88f2c8e76
Jason Cabrera <cabrerajason7@gmail.com>
2021-02-19 21:28:16 -0800

    added more info to motw

--- client/src/Home.js
+++ client/src/Home.js
@@ -89,6 +89,9 @@ class Home extends React.Component {
               <div className="description-container">
                 <p style={{marginBottom: '-.5px'}}> Description: </p>
                 <p> Jack Torrance (Jack Nicholson) becomes winter caretaker at the isolated Overlook Hotel in Colorado, hoping to cure his writer's block. He settles in along with his wife, Wendy (Shelley Duvall), and his son, Danny (Danny Lloyd), who is plagued by psychic premonitions. As Jack's writing goes nowhere and Danny's visions become more disturbing, Jack discovers the hotel's dark secrets and begins to unravel into a homicidal maniac hell-bent on terrorizing his family. </p>
+
+                <p> Genre: Drama, Horror | Runtime: 146 min | Rated R</p>
+
               </div>
             </div> 
             : 
