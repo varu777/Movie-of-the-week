a08068a7ad3bdfecad5e45abc944f164ff2ab13c
Jason Cabrera <cabrerajason7@gmail.com>
2021-02-19 20:33:07 -0800

    updated movieotw container ordering

--- client/src/Home.js
+++ client/src/Home.js
@@ -57,7 +57,7 @@ class Home extends React.Component {
     // sort watched movies array by new filter
     axios({
       method: 'post',
-      url: "https://movieotw.herokuapp.com/SortWatched",
+      url: "http://localhost:9000/SortWatched",
       data: {
           sortBy: sortBy
       }
@@ -79,16 +79,16 @@ class Home extends React.Component {
         <CustomNavbar />
         {this.state.isMovieSelected ? 
             <div className="motw-container borders"> 
-              <h1 className="title"> Movie of the Week</h1>
-              <h3> Friday | February 19, 2021</h3>
-              <img style={{marginTop: '1vw', height: '45%', width: '45%'}} src="https://m.media-amazon.com/images/M/MV5BZWFlYmY2MGEtZjVkYS00YzU4LTg0YjQtYzY1ZGE3NTA5NGQxXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"></img> 
               <h1 className="title"> {this.state.movieOTW} </h1>
-              <div style={{width: '80%', margin: 'auto', textAlign: 'left'}}>
+              <h4> February 19, 2021</h4>
+              <p > Location: <a href="https://zoom.us/j/97457711739?pwd=Z2x3K3l5OUVTQVJmNDBkRGNqWHdjZz09">Zoom Theatre</a></p> 
+              <p style={{marginTop: '-.5vw'}}> Added by {this.state.userOTW} </p>
+              {this.state.noteOTW.length === 0 ? null : <p> Teaser: {this.state.noteOTW} </p>}
+              <img style={{height: '45%', width: '45%'}} src="https://m.media-amazon.com/images/M/MV5BZWFlYmY2MGEtZjVkYS00YzU4LTg0YjQtYzY1ZGE3NTA5NGQxXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"></img> 
+              <div className="bottom-text" style={{width: '80%', margin: 'auto', textAlign: 'left', marginTop: '2vw'}}>
+                <p style={{marginBottom: '-.5px'}}> Description: </p>
                 <p> Jack Torrance (Jack Nicholson) becomes winter caretaker at the isolated Overlook Hotel in Colorado, hoping to cure his writer's block. He settles in along with his wife, Wendy (Shelley Duvall), and his son, Danny (Danny Lloyd), who is plagued by psychic premonitions. As Jack's writing goes nowhere and Danny's visions become more disturbing, Jack discovers the hotel's dark secrets and begins to unravel into a homicidal maniac hell-bent on terrorizing his family. </p>
               </div>
-              <p> Added by {this.state.userOTW} </p>
-              {this.state.noteOTW.length === 0 ? null : <p> Teaser: {this.state.noteOTW} </p>}
-              <p className="bottom-text"> Watch it <a href="https://zoom.us/j/97457711739?pwd=Z2x3K3l5OUVTQVJmNDBkRGNqWHdjZz09">here</a></p> 
             </div> 
             : 
             <p> No movie selected yet for this week.</p>
