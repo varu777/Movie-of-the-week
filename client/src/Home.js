3f6eadfc5f5aece03e7863824eaab3231431dfd1
Jason Cabrera <cabrerajason7@gmail.com>
2021-03-06 20:15:15 -0800

    updated movie

--- client/src/Home.js
+++ client/src/Home.js
@@ -81,16 +81,16 @@ class Home extends React.Component {
         {this.state.isMovieSelected ? 
             <div className="motw-container borders"> 
               <h1 className="title"> {this.state.movieOTW} </h1>
-              <h4> February 26, 2021</h4>
+              <h4> March 6, 2021</h4>
               <p> Location: <a style={{textDecoration: 'underline'}}href="https://zoom.us/j/97457711739?pwd=Z2x3K3l5OUVTQVJmNDBkRGNqWHdjZz09">Zoom Theatre</a></p> 
               <p className="addedBy"> Added by {this.state.userOTW} </p>
               {this.state.noteOTW.length === 0 ? null : <p> Teaser: {this.state.noteOTW} </p>}
-              <img style={{height: '45%', width: '45%'}} src="https://m.media-amazon.com/images/M/MV5BOTYwNWNmM2UtNDhlOC00ZGQzLWI1MTMtMmZlMTFjM2Y1N2ZhXkEyXkFqcGdeQXVyMTY5Nzc4MDY@._V1_SX300.jpg"></img> 
+              <img style={{height: '45%', width: '45%'}} src="https://m.media-amazon.com/images/M/MV5BZjU5OWVlN2EtODNlYy00MjhhLWI0MDUtMTA3MmQ5MGMwYTZmXkEyXkFqcGdeQXVyNjE5MTM4MzY@._V1_SX300.jpg"></img> 
               <div className="description-container">
                 <p style={{marginBottom: '-.5px'}}> Description: </p>
-                <p> FBI informant William O'Neal infiltrates the Illinois Black Panther Party and is tasked with keeping tabs on their charismatic leader, Chairman Fred Hampton. A career thief, O'Neal revels in the danger of manipulating both his comrades and his handler, Special Agent Roy Mitchell. Hampton's political prowess grows just as he's falling in love with fellow revolutionary Deborah Johnson. Meanwhile, a battle wages for O'Neal's soul. Will he align with the forces of good? Or subdue Hampton and The Panthers by any means, as FBI Director J. Edgar Hoover commands? </p>
+                <p> Ed and Lorraine Warren travel to North London to help a single mother raising four children alone in a house plagued by a supernatural spirit. </p>
 
-                <p> Genre: Biography, Drama, History | Runtime: 126 min | Rated R</p>
+                <p> Genre: Horror, Mystery, Thriller | Runtime: 134 min | Rated R</p>
 
               </div>
             </div> 
